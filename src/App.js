66f6c8121299b060659cf5ce31c77f1e5698bee3
vinyasbj <vinyas555@gmail.com>
2018-06-10 00:14:32 +0530

    we can add article now

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 // import {BrowserRouter, Route} from 'react-router-dom';
-// import Article from "./components/Articles";
 import Articles from "./components/Articles";
 import "./css/bootstrap.css";
 import "./css/bootstrap-grid.css";
 import "./css/bootstrap-grid.min.css";
 import "./css/bootstrap-reboot.min.css";
 
+const API_URL = 'http://localhost:8000/api/v1';
 class App extends Component {
-
     state = {
       articles: [],
     }
     componentDidMount(){
-      return fetch('https://react-blogapi.herokuapp.com/api/v1/articles')
+      return fetch(`${API_URL}/articles`)
       .then((Response) => Response.json())
       .then((responseJson) => {
-        console.log(responseJson)
         this.setState({
           articles: responseJson
         })
@@ -25,35 +23,35 @@ class App extends Component {
 
   render() {
     return (
-     <div>
-         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-          <div class="container">
-            <a class="navbar-brand" href="#">Blog</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home
-                    <span class="sr-only">(current)</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Services</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Contact</a>
-                </li>
-              </ul>
+       <div>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div className="container">
+              <a className="navbar-brand" href="">Blog</a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="">Home
+                      <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="">About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="">Services</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="">Contact</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-      <Articles articles = {this.state.articles}/>
-     </div>
+          </nav>
+        <Articles articles = {this.state.articles}/>
+       </div>
     );
   }
 }
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,16 +1,57 @@
 import React from 'react' ; 
-
+// const API_URL = `http://localhost:8000/api/v1/articles`;
+const API_URL = `https://react-blogapi.herokuapp.com/api/v1/articles`;
 class Articles extends React.Component {
+	state = {
+	id: undefined,
+    title: undefined,
+    body: undefined,
+    category: undefined,
+    error: undefined
+  }
+
+	addArticle = (e) => {
+    	e.preventDefault();
+    	console.log(this.refs.title.value);
+    	// this.setState({
+    	// 	title: this.refs.title.value,
+    	// 	body: this.refs.body.value
+    	// })
+
+		fetch(`${API_URL}`, {
+		  method: "post",
+		  headers: {
+		    'Accept': 'application/json',
+		    'Content-Type': 'application/json'
+		  },
+
+		  //make sure to serialize your JSON body
+		  body: JSON.stringify({
+		    title: this.refs.title.value,
+		    body: this.refs.body.value
+		  })
+		})
+		.then( (response) => { 
+		   //do something awesome that makes the world a better place
+		});
+	}
+
+	deleteArticle = (e) => {
+		e.preventDefault();
+		console.log(this);
+		// this.setState({
+  //   		id: 1
+  //   	})
+	}
+
  	render() {
  		return (
- 				
 				<div className="container">
 					<div className="row">
 						<div className="col-md-8">
 						<h1 className="my-4">Articles
-							<small>blogres</small>
+							<small></small>
 						</h1>
-
 						{this.props.articles.map(obj => {
 					    return (
 					    	<div className="card mb-4">
@@ -20,13 +61,27 @@ class Articles extends React.Component {
 								    	{obj.title}
 								    </h1>
 									<p className="card-text">{obj.body}</p>
-									<a href="#" class="btn btn-primary">Read More &rarr;</a>
+									<a href="" className="btn btn-primary">Read More &rarr;</a>
+									<button onClick= {this.deleteArticle.bind(obj.id)} className="btn btn-danger" type= "delete">Delete &rarr;</button>
 					    		</div>
 					        </div>
 					        )
 					    })}
 					</div>
-				</div>
+					<div className="col-md-4">
+			          <div className="card my-4">
+			            <h5 className="card-header">Add Articles</h5>
+			            <div className="card-body">
+			              <form onSubmit= {this.addArticle.bind(this)}>
+							<input type="text" name= "title" ref="title" placeholder= "Title"/>
+							<br/>
+							<textarea rows="4" cols="30" type="text" name= "body" ref="body" placeholder= "Body"/>
+							<button className="btn btn-primary">Add Article</button>
+						</form>
+			            </div>
+			          </div>
+			        </div>
+			        </div>
 				</div>
 			
  		);
